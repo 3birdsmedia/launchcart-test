a7b53a6b00f9afab7ef63a191b18d06037b13ff6
Marco Segura <marco@leadid.net>
2020-06-06 10:25:18 -0700

    Added Contacts model and db-migration for contacts, also added default string lenght to fix an old MySQL error

---
+++ app/Contact.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    // This is the structure for Klaviyo's contacts
+    protected $fillable = [
+        'first_name',
+        'email',
+        'phone',
+     ];
+
+
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Set a default to fix the migrations error "Specified key was too long error"
+        Schema::defaultStringLength(191);
+    }
+}
